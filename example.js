800c49e376cf1f1a0ea46e93457f471a08bb6d2a
dylan huff <dylanhuff@dylans-MacBook-Pro.local>
2018-10-22 15:11:14 -0700

    Scripts to compile, run and make added

---
+++ example.js
@@ -0,0 +1,22 @@
+function testSave(){
+	var canvas = document.createElement('canvas');
+	canvas.width = '716';
+	canvas.height = '537';
+	canvas.style.border = '1px solid';
+	var body = document.getElementsByTagName('body')[0];
+	body.appendChild(canvas);
+	var ctx = canvas.getContext('2d');
+	ctx.rect(400,0,100,100);
+	ctx.fillStyle = '#ff0000';
+	ctx.fillRect(400,0,100,100);
+	ctx.rect(100,100,100,100);
+	ctx.fillStyle = '#000000';
+	ctx.fillRect(100,100,100,100);
+	ctx.rect(300,200,300,100);
+	ctx.fillStyle = '#ffff00';
+	ctx.fillRect(300,200,300,100);
+	ctx.rect(0,200,300,100);
+	ctx.fillStyle = '#ffc800';
+	ctx.fillRect(0,200,300,100);
+	ctx.stroke();
+}
