7b74b5cab89c9dbfa4e0bfc6fdb7f6f3ebc8f009
dylan huff <dylanhuff@dylans-MacBook-Pro.local>
2018-10-24 09:59:55 -0700

    actual changes, reorganizing files

--- JavaPPTX/src/edu/stanford/cs/pptx/PPSaveJS.java
+++ JavaPPTX/src/edu/stanford/cs/pptx/PPSaveJS.java
@@ -5,7 +5,6 @@
  * PowerPoint show to a .js and .html file.
  */
 
- //should be comitting this to github 
 package edu.stanford.cs.pptx;
 
 import java.io.File;
@@ -23,6 +22,7 @@ public class PPSaveJS {
         
         // assuming one slide for now
         //int nSlides = show.getSlideCount();
+        // please work
 
         if (filename.startsWith("~/")) {
             filename = System.getProperty("user.home") + filename.substring(1);
@@ -39,6 +39,7 @@ public class PPSaveJS {
             slide = show.getSlide(1); //currently assuming only 1 slide
             PPShape[] shapes = new PPShape[slide.getShapes().length];
             shapes = slide.getShapes();
+            PPDumpShapeJS dump = new PPDumpShapeJS();
             //better function name then testSave (change in test.html as well)
             fop.write("function testSave(){\n".getBytes()); //I set the canvas side to the slide size, maybe should be GWindow size? 
             //gwindow could be added as a sepearte smaller window
@@ -53,63 +54,13 @@ public class PPSaveJS {
             fop.write("\tvar body = document.getElementsByTagName('body')[0];\n".getBytes());
             fop.write("\tbody.appendChild(canvas);\n".getBytes());
             fop.write("\tvar ctx = canvas.getContext('2d');\n".getBytes());
-            //how about
-            //not sure if there is a better way to get correct length for rectShapes array
             //add a couple objects then start adding animations
             //add move animations, along curves
-            int i = 0;
-            for(PPShape shape : shapes){
-                if(shape.getTypeName().equals("PPRect")){
-                    i+=1;
-                }
-            }
-
-            PPRect[] rectShapes = new PPRect[i];
-
-            i = 0;
-            for(PPShape shape : shapes){
-                if(shape.getTypeName().equals("PPRect")){
-                    rectShapes[i] = (PPRect)shape;
-                    i+=1;
-                }
-            }
+            dump.writeShapes(fop, shapes);
+            
             //it will be in the order of the display list
             //go throught the object list backl to front, then calls something to display
-            //
-            //this should be broken out into a seperate file
-            //should be a shapesDump file with differnet methods for every shape (?)
-            //methods take fop, no return
-            for(PPRect shape : rectShapes){
-                fop.write("\tctx.rect(".getBytes());
-                fop.write(String.valueOf((int)shape.getX()).getBytes());
-                fop.write(",".getBytes());
-                fop.write(String.valueOf((int)shape.getY()).getBytes());
-                fop.write(",".getBytes());
-                fop.write(String.valueOf((int)shape.getWidth()).getBytes());
-                fop.write(",".getBytes());
-                fop.write(String.valueOf((int)shape.getHeight()).getBytes());
-                fop.write(");\n".getBytes());
-                
-                if(!(shape.getFillColor().equals(null))){ //this is a problem because black is the default but could also be set
-
-                    fop.write("\tctx.fillStyle = '".getBytes());
-                    String hex = "#"+Integer.toHexString(shape.getFillColor().getRGB()).substring(2);
-                    fop.write(hex.getBytes());
-                    fop.write("';\n".getBytes());
-                    fop.write("\tctx.fillRect(".getBytes());
-                    fop.write(String.valueOf((int)shape.getX()).getBytes());
-                    fop.write(",".getBytes());
-                    fop.write(String.valueOf((int)shape.getY()).getBytes());
-                    fop.write(",".getBytes());
-                    fop.write(String.valueOf((int)shape.getWidth()).getBytes());
-                    fop.write(",".getBytes());
-                    fop.write(String.valueOf((int)shape.getHeight()).getBytes());
-                    fop.write(");\n".getBytes());
-
-                }
-                
-            }
-
+            
             fop.write("\tctx.stroke();\n".getBytes());
 
             fop.write("}".getBytes());
--- test/src/testRectSave.java
+++ test/src/testRectSave.java
@@ -12,15 +12,21 @@ public class testRectSave {
         PPRect testRect = new PPRect(400,0,100,100);
         PPRect testRect2 = new PPRect(100,100,100,100);
         PPRect testRect3 = new PPRect(300,200,300,100);
-        PPRect testRect4 = new PPRect(000,200,300,100);
+        PPRect testRect4 = new PPRect(200,400,100,100);
         testRect.setFillColor(Color.BLACK);
-	    testRect3.setFillColor(Color.BLACK);
-	    testRect4.setFillColor(Color.RED);
+        testRect2.setFillColor(Color.YELLOW);
+	    testRect3.setFillColor(Color.BLUE);
+        testRect4.setFillColor(Color.RED);
+        
+        PPLine testLine = new PPLine(0, 0, 300, 300);
+        PPLine testLine2 = new PPLine(100, 300, 100, 300);
 
         slide.add(testRect);
         slide.add(testRect2);	
         slide.add(testRect3);
         slide.add(testRect4);
+        slide.add(testLine);
+        slide.add(testLine2);
         testShow.add(slide);
 
         PPSaveJS testSave = new PPSaveJS(testShow);
