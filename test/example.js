ed4cb9dd4f2aed31e0bb92f161dd4006bf374d10
dylan huff <dylanhuff@dylans-MacBook-Pro.local>
2018-11-20 16:25:57 -0800

    Added support for multiple onClicks

--- test/example.js
+++ test/example.js
@@ -1,4 +1,4 @@
-function testSave(){
+function testSave() {
 	var canvas = document.createElement('canvas');
 	canvas.width = '716';
 	canvas.height = '537';
@@ -7,24 +7,48 @@ function testSave(){
 	body.appendChild(canvas);
 	var ctx = canvas.getContext('2d');
 	ctx.beginPath();
-	ctx.ellipse(300.0,300.0,100.0,50.0,0,0,6.283185307179586);
+	ctx.ellipse(300.0, 300.0, 100.0, 50.0, 0, 0, 6.283185307179586);
 	ctx.fillStyle = '#ffffff';
 	ctx.fill();
-	canvas.addEventListener('click', function() {
-		ctx.rect(400,0,100,100);
-		ctx.fillStyle = '#000000';
-		ctx.fillRect(400,0,100,100);
-	}, false);
-	ctx.rect(100,100,100,100);
-	ctx.fillStyle = '#ffff00';
-	ctx.fillRect(100,100,100,100);
-	ctx.rect(300,200,300,100);
-	ctx.fillStyle = '#0000ff';
-	ctx.fillRect(300,200,300,100);
-	ctx.rect(200,400,100,100);
-	ctx.fillStyle = '#ff0000';
-	ctx.fillRect(200,400,100,100);
-	ctx.moveTo(300,300);
-	ctx.lineTo(0,0);
+	var counter = 0;
+	canvas.addEventListener('click', click0);
+	function click0() {
+		if (counter == 0) {
+			ctx.rect(400, 0, 100, 100);
+			ctx.fillStyle = '#000000';
+			ctx.fillRect(400, 0, 100, 100);
+			counter += 1;
+			canvas.addEventListener('click', click1);
+		}
+	}
+	function click1() {
+		if (counter == 1) {
+			ctx.rect(100, 100, 100, 100);
+			ctx.fillStyle = '#ffff00';
+			ctx.fillRect(100, 100, 100, 100);
+			counter += 1;
+			canvas.addEventListener('click', click2);
+		}
+	}
+	function click2() {
+		if (counter == 2) {
+			ctx.rect(300, 200, 300, 100);
+			ctx.fillStyle = '#0000ff';
+			ctx.fillRect(300, 200, 300, 100);
+			counter += 1;
+			canvas.addEventListener('click', click3);
+		}
+	}
+	function click3() {
+		if (counter == 3) {
+			ctx.rect(200, 400, 100, 100);
+			ctx.fillStyle = '#ff0000';
+			ctx.fillRect(200, 400, 100, 100);
+			counter += 1;
+			canvas.addEventListener('click', click4);
+		}
+	}
+	ctx.moveTo(300, 300);
+	ctx.lineTo(0, 0);
 	ctx.stroke();
 }
